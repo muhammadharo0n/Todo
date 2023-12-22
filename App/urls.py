7075fb7299e46f42fbf72ae55a0f82d67ef14301
unknown <muhammadharo0n>
2023-12-22 15:10:35 +0500

    Final Addings in TODO APP

--- App/urls.py
+++ App/urls.py
@@ -3,5 +3,19 @@ from . import views
 
 urlpatterns = [
     path ('', views.home , name= 'home'),
+    
+    # add task
     path ('todo/', views.addtask , name= 'addtask'),
+    
+    # mark as done
+    path ('done/<int:pk>/', views.done , name='done'),
+
+    # mark as done
+    path ('undone/<int:pk>/', views.undone , name="undone"),
+
+    #edit feature
+    path ('edit/<int:pk>/', views.edit , name="edit"),
+
+    #delete Task
+    path ('delete/<int:pk>/', views.delete , name="delete")
 ]
--- App/views.py
+++ App/views.py
@@ -1,15 +1,55 @@
-from django.shortcuts import render , redirect
+from django.shortcuts import get_object_or_404, render , redirect
 from django.http import HttpResponse
 from App.models import Task
 
 # Create your views here.
 
-def home(request): 
+def home(request):
+
     task = Task.objects.filter(is_completed =False)
-    context = {'task':task}
+    completed_tasks = Task.objects.filter(is_completed =True)
+    context = {
+        'task':task,
+        'completed_tasks':completed_tasks
+        }
     return render(request , 'home.html', context)
 
 def addtask(request):
     task = request.POST['task']
     Task.objects.create(task = task)
     return redirect ('home')
+
+def done(request ,pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def undone (request , pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect ('home')
+
+def edit(request ,pk):
+    get_task = get_object_or_404(Task , pk=pk)
+    if request.method=='POST':
+        new_task = request.POST['task']
+        get_task.task = new_task
+        get_task.save()   
+        return redirect ('home')
+     
+    else:
+        context = {
+            'get_task':get_task
+            }
+        return render (request , 'edit.html' , context)
+    
+def delete(request,pk):
+    task = get_object_or_404(Task,pk=pk)
+    task.delete()
+    return redirect ('home')
+
+    
+    
+    
